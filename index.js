25befdf90a23d5355adb46ff05c75b3a35b0eb5e
Keith Arnold <keith.arnold@asu.edu>
2018-10-16 15:27:49 -0700

    added worker process

--- index.js
+++ index.js
@@ -12,16 +12,4 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM mcsandbox.contact LIMIT 10');
-      const results = {'results': (result) ? result.rows : null};
-      res.render('pages/db', results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ worker.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const {Pool} = require('pg');
+const pool = new Pool({
+  connectionString: process.env.HEROKU_POSTGRESQL_NAVY_URL,
+  ssl: true
+})
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM mcsandbox.contact LIMIT 10');
+      const results = {'results': (result) ? result.rows : null};
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
