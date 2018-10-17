66849bf86104debca5e16a009627058f9b8d2afa
Keith Arnold <keith.arnold@asu.edu>
2018-10-17 08:48:32 -0700

    broke out fucntions based on example

--- worker.js
+++ worker.js
@@ -1,21 +1,18 @@
 const {Pool} = require('pg');
 const pool = new Pool({
   connectionString: process.env.HEROKU_POSTGRESQL_NAVY_URL,
-  ssl: true,
-  function(err, client, done) {
-    console.log('Log Message');
-    if (err) {
-      console.error(err)
-      process.exit(1);
-    }
-    client.query('SELECT * FROM mcsandbox.contact ORDER BY CreatedDate LIMIT 10', function(err, contacts) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(contacts);
-      }
-    });
+  ssl: true
+});
+
+console.log('Connect to db');
+pool.connect();
+
+pool.query('SELECT * FROM mcsandbox.contact ORDER BY CreatedDate LIMIT 10', function(err, contacts) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(contacts);
   }
 });
 
-pool.connect();
+pool.end();
