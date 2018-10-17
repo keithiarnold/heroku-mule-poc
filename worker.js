e9cda12db7066df5b8c430d652bf2c7af9cabfb8
Keith Arnold <keith.arnold@asu.edu>
2018-10-17 08:35:43 -0700

    Changed worker to simple example

--- worker.js
+++ worker.js
@@ -1,40 +1,8 @@
-/*const {Pool} = require('pg');
-const pool = new Pool({
-  connectionString: process.env.HEROKU_POSTGRESQL_NAVY_URL,
-  ssl: true,
-  function(err, client, done) {
-    console.log('Log Message');
-    if (err) {
-      console.error(err)
-      process.exit(1);
-    }
-    client.query('SELECT * FROM mcsandbox.contact ORDER BY CreatedDate LIMIT 10', function(err, contacts) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(contacts);
-      }
-    });
-  }
-});*/
+const {Client} = require('pg')
+const client = new Client()
 
-import { connect } from 'pg';
+await client.connect()
 
-connect({
-  connectionString: process.env.HEROKU_POSTGRESQL_NAVY_URL,
-  ssl: true,
-  function(err, client, done) {
-    console.log('Log Message');
-    if (err) {
-      console.error(err)
-      process.exit(1);
-    }
-    client.query('SELECT * FROM mcsandbox.contact ORDER BY CreatedDate LIMIT 10', function(err, contacts) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(contacts);
-      }
-    });
-  }
-});
+const res = await createInterface.query('SELECT * FROM mcsandbox.contact ORDER BY CreatedDate LIMIT 10')
+console.log(res.rows)
+await client.end()
